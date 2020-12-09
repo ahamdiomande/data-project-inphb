30fffb4f3635757c4a9f1059211069fa1a6afb7d
ahamdiomande <aahdiomande@gmail.com>
2020-12-09 11:18:03 +0000

    ENH config: adding requirement.txt and rm useless files

--- data_project_inphb/Preprocessing/convert_cas2.py
+++
@@ -1,8 +0,0 @@
-def convert_cast(df, categorical_cols):
-    for col_name in categorical_cols:
-        df[col_name] = df[col_name].astype("object")
-    return df
-
-categorical_cols = ["Survived", "Pclass"]
-df= convert_cast(train, categorical_cols)
-df.info()
